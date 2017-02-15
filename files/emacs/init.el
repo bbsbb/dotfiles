940b76b94cf53364aeca4719bf42e62d8a5d51fe
Boyan Bonev <b.bonev@redbuffstudio.com>
2017-02-15 14:49:59 +0100

    Initial org-mode configuration.

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -59,6 +59,12 @@
 (setq ac-ignore-case nil)
 (ac-config-default)
 ;;;;;;;;;;;;;;;;;;END;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;ORG MOD;;;;;;;;;;;;;;;;;;;;;;;;
+(setq org-directory "~/Dropbox/org")
+(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+(setq org-mobile-inbox-for-pull "~/Dropbox/org/inbox.org")
+(setq org-mobile-files '("~/Dropbox/org"))
+;;;;;;;;;;;;;;;;;;END;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;Enable paredit for some modes;;
 (defun activate-paredit-mode-custom ()
   (interactive)
